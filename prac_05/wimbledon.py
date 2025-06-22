e23016c00b51bf7ad1bab28969188b55be0a99ed
administrator <tzhang2024@gmail.com>
2025-06-22 17:08:01 +0800

    Count champion wins

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,13 +3,22 @@ FILENAME = "wimbledon.csv"
 def main():
     champions = []
     with open(FILENAME, "r", encoding="utf-8-sig") as file:
-        next(file)  # Skip header
+        next(file)
         for line in file:
             parts = line.strip().split(",")
             champion = parts[2]
             country = parts[1]
             champions.append((champion, country))
 
-    print(champions[:5])  # For quick check
+    champion_to_count = {}
+    for champion, _ in champions:
+        if champion in champion_to_count:
+            champion_to_count[champion] += 1
+        else:
+            champion_to_count[champion] = 1
+
+    print("Wimbledon Champions:")
+    for champion, count in sorted(champion_to_count.items()):
+        print(f"{champion} {count}")
 
 main()
