afce62da516fd9175cab6212e1655dd6dd5d0db6
administrator <tzhang2024@gmail.com>
2025-06-22 16:27:10 +0800

    word count

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,13 @@
+text = input("Text: ")
+words = text.split()
+
+word_to_count = {}
+for word in words:
+    word = word.lower()
+    if word in word_to_count:
+        word_to_count[word] += 1
+    else:
+        word_to_count[word] = 1
+
+for word, count in word_to_count.items():
+    print(f"{word} : {count}")
