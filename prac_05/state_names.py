26318952be7b1d5d58d5f6c6677347beac898233
administrator <tzhang2024@gmail.com>
2025-06-22 16:07:00 +0800

    allow lower case

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -17,10 +17,10 @@ CODE_TO_NAME = {
 
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
