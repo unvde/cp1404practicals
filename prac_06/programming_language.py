16a2aeafc9e271007d3c11ee9fea7b82da08138a
administrator <tzhang2024@gmail.com>
2025-07-05 03:46:30 +0800

    Add ProgrammingLanguage class with __init__

---
+++ prac_06/programming_language.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 Practical
+programming_language.py
+Class for representing a programming language's key attributes.
+"""
+
+
+class ProgrammingLanguage:
+    """Represent a programming language."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a programming language instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
