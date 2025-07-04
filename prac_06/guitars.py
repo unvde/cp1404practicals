c69a4cddfeba4f64ad3025113fb435597a431ad5
administrator <tzhang2024@gmail.com>
2025-07-05 04:48:32 +0800

    Add user input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,21 +1,28 @@
 """
 CP1404/CP5632 Practical
 guitars.py
-Program to manage and display a list of guitars.
+Program to manage and display a list of guitars entered by the user.
 """
 
 from guitar import Guitar
 
 
 def main():
-    """Display a list of predefined guitars."""
-    guitars = [
-        Guitar("Gibson L-5 CES", 1922, 16035.40),
-        Guitar("Line 6 JTV-59", 2010, 1512.90),
-        Guitar("Fender Stratocaster", 2014, 765.40)
-    ]
+    """Allow user to enter guitars, then display them."""
+    guitars = []
 
-    print("These are my guitars:")
+    print("My guitars!")
+    name = input("Name: ")
+    while name:
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
+
+    # Output
+    print("\nThese are my guitars:")
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
         print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
