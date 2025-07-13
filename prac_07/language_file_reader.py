8201a924229ff936b27e104f2f5e74d0b2c2dd0e
administrator <tzhang2024@gmail.com>
2025-07-13 14:08:24 +0800

    Finalise main() to parse and pass pointer_arithmetic field

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -13,30 +13,19 @@ from programming_language import ProgrammingLanguage
 def main():
     """Read file of programming language details, save as objects, display."""
     languages = []
-    # Open the file for reading
     in_file = open('languages.csv', 'r')
-    # File format is like: Language,Typing,Reflection,Year
-    # 'Consume' the first line (header) - we don't need its contents
     in_file.readline()
-    # All other lines are language data
     for line in in_file:
-        # print(repr(line))  # debugging
-        # Strip newline from end and split it into parts (CSV)
         parts = line.strip().split(',')
-        # print(parts)  # debugging
-        # Reflection is stored as a string (Yes/No) and we want a Boolean
         reflection = parts[2] == "Yes"
-        # Construct a ProgrammingLanguage object using the elements
-        # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
-        # Add the language we've just constructed to the list
+        pointer_arithmetic = parts[4] == "Yes"
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
         languages.append(language)
-    # Close the file as soon as we've finished reading it
     in_file.close()
 
-    # Loop through and display all languages (using their str method)
     for language in languages:
         print(language)
+
 
 
 main()
