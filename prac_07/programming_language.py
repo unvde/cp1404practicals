4b4d5a9864c84e41c08e008ba2dccf225015d7a5
administrator <tzhang2024@gmail.com>
2025-07-13 13:51:55 +0800

    Update ProgrammingLanguage to include pointer_arithmetic

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,16 +7,18 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, "
+                f"Pointer Arithmetic={self.pointer_arithmetic}, First appeared in {self.year}")
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
@@ -25,11 +27,12 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, False)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
+    cplusplus = ProgrammingLanguage("C++", "Static", False, 1983, True)
 
-    languages = [ruby, python, visual_basic]
+    languages = [ruby, python, visual_basic, cplusplus]
     print(python)
 
     print("The dynamically typed languages are:")
