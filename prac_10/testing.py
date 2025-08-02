d6dd603c96e2a355f647c2064a77008b470e909a
administrator <tzhang2024@gmail.com>
2025-08-03 00:43:58 +0800

    Step 1: Fix repeat_string() to return spaced repetition using join

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -32,9 +32,6 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
-
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
