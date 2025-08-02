a1d1314f110f0fdd406d44489e381eda08796fd8
administrator <tzhang2024@gmail.com>
2025-08-03 01:15:37 +0800

    Step 3: Add error handling for PageError and DisambiguationError in wiki.py

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,6 +3,7 @@ Wikipedia search interface - user input loop.
 """
 
 import wikipedia
+from wikipedia.exceptions import DisambiguationError, PageError
 
 def main():
     """Prompt for Wikipedia page titles and display basic info."""
@@ -11,10 +12,16 @@ def main():
         if not title:
             print("Thank you.")
             break
-        page = wikipedia.page(title)
-        print(page.title)
-        print(page.summary)
-        print(page.url)
+        try:
+            page = wikipedia.page(title, auto_suggest=False)
+            print(page.title)
+            print(page.summary)
+            print(page.url)
+        except PageError:
+            print(f'Page id "{title}" does not match any pages. Try another id!')
+        except DisambiguationError as e:
+            print("We need a more specific title. Try one of the following:")
+            print(e.options)
 
 if __name__ == "__main__":
     main()
